6703d01e7d1d55ec03f37c7c0f8b2d19e4ecadeb
Levin <vbatallones.xvi@gmail.com>
2020-11-23 23:48:56 -0800

    add file structure

---
+++ todos_list/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ToDo from './components/todo/form.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <ToDo />
+      </>
+    );
+  }
+}
